99d4649ff026a72f9cda56c41752b4e021f73d1d
dabear <bjorninge@bjorninge.no>
2017-09-24 18:15:46 +0200

    whitespace

--- getlightcolors.py
+++ getlightcolors.py
@@ -11,4 +11,6 @@ all_lights = b.get_light_objects('name')
 for name, light in all_lights.items():
     print("Lamp light {0}: {1}".format(light.name, light.xy))
     if not light.reachable:
-        print("Lamp light currently not reachable!\n")
+        print("Lamp light currently not reachable!")
+        
+    print("")
